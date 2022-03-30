343b984100b4e755acb4ce2985b62a2d7c277229
velmurugan-murugesan <velmurugan.murugesan@gmail.com>
2022-03-30 10:33:36 +0530

    added form

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/form")
+@app.route("/form", methods=["POST"])
+def form():
+
+    if request.method == "POST":
+        return render_template('form.html', error="post")
+
+    return render_template('form.html')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
